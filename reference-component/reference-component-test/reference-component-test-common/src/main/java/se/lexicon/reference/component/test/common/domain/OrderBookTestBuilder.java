78341f7db5e20bb21160ce1d983dd41649206441
shazahlaleh <shazahilalh@gmail.com>
2019-08-08 15:35:04 +0200

    create OrderBook domain class and tests

---
+++ reference-component/reference-component-test/reference-component-test-common/src/main/java/se/lexicon/reference/component/test/common/domain/OrderBookTestBuilder.java
@@ -0,0 +1,25 @@
+package se.lexicon.reference.component.test.common.domain;
+
+import com.so4it.common.util.object.Required;
+import com.so4it.test.domain.AbstractTestBuilder;
+import se.lexicon.reference.component.domain.OrderBook;
+
+public class OrderBookTestBuilder extends AbstractTestBuilder<OrderBook> {
+
+    private OrderBook.Builder builder;
+
+    private OrderBookTestBuilder(OrderBook.Builder builder) {
+        this.builder = Required.notNull(builder,"builder");
+        this.builder = builder.withId("12345")
+                .withName("test");
+    }
+
+    public static OrderBookTestBuilder builder() {
+        return new OrderBookTestBuilder(OrderBook.builder());
+    }
+
+    @Override
+    public OrderBook build() {
+        return builder.build();
+    }
+}

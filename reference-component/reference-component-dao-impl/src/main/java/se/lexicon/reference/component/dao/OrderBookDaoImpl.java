ebf28907d209bcae483aade35416afe8d882659b
shazahlaleh <shazahilalh@gmail.com>
2019-08-09 15:19:00 +0200

    Created OrderBookDao and its Impl.

---
+++ reference-component/reference-component-dao-impl/src/main/java/se/lexicon/reference/component/dao/OrderBookDaoImpl.java
@@ -0,0 +1,11 @@
+package se.lexicon.reference.component.dao;
+
+import com.so4it.component.dao.gs.AbstractSpaceDao;
+import org.openspaces.core.GigaSpace;
+import se.lexicon.reference.component.entity.OrderBookEntity;
+
+public class OrderBookDaoImpl extends AbstractSpaceDao<OrderBookEntity ,String> implements OrderBookDao {
+    public OrderBookDaoImpl(GigaSpace gigaSpace) {
+        super(gigaSpace);
+    }
+}
---
+++ reference-component/reference-component-test/reference-component-test-common/src/main/java/se/lexicon/reference/component/test/common/entity/OrderBookEntityTestBuilder.java
@@ -0,0 +1,23 @@
+package se.lexicon.reference.component.test.common.entity;
+
+import com.so4it.common.util.object.Required;
+import com.so4it.test.domain.AbstractTestBuilder;
+import se.lexicon.reference.component.entity.OrderBookEntity;
+
+public class OrderBookEntityTestBuilder extends AbstractTestBuilder<OrderBookEntity> {
+
+  private OrderBookEntity.Builder builder;
+
+  public OrderBookEntityTestBuilder(OrderBookEntity.Builder builder){
+      this.builder= Required.notNull(builder ,"builder");
+      this.builder=builder.withId("1234").withName("shaza");
+  }
+  public static OrderBookEntityTestBuilder builder(){
+      return new OrderBookEntityTestBuilder(OrderBookEntity.builder());
+  }
+    @Override
+    public OrderBookEntity build() {
+        return builder.build();
+    }
+
+}

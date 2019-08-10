ae456d78a6fa9a59490fd020d8db9dd46de71988
evajavadev <evajavadev@gmail.com>
2019-08-10 19:18:50 +0200

    Renamed ReferenceComponentOrderBookEntityUnitTest class name, to ReferenceComponentDomainUnitTest

--- reference-component/reference-component-test/reference-component-test-unit/src/test/java/se/lexicon/account/component/test/unit/ReferenceComponentDomainUnitTest.java
+++ reference-component/reference-component-test/reference-component-test-unit/src/test/java/se/lexicon/account/component/test/unit/ReferenceComponentDomainUnitTest.java
@@ -1,19 +1,17 @@
 package se.lexicon.account.component.test.unit;
 
-import com.so4it.serialization.jackson.ObjectMapperFactory;
-import com.so4it.test.builder.domain.DomainMatchers;
+import com.so4it.test.builder.entity.EntityMatchers;
 import com.so4it.test.category.UnitTest;
 import com.so4it.test.domain.TestBuilderExecutor;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(UnitTest.class)
-public class ReferenceComponentDomainUnitTest {
-
-    private static final String PACKAGE_NAME = "se.lexicon.reference.component.test.common.domain";
+public class ReferenceComponentDomainUnitTest  {
+    private static final String PACKAGE_NAME = "se.lexicon.reference.component.test.common.entity";
 
     @Test
-    public void testDomainCompliance() {
-        TestBuilderExecutor.execute(PACKAGE_NAME, DomainMatchers.builder().withObjectMapper(ObjectMapperFactory.instance()).build());
+    public void testEntityCompliance() {
+        TestBuilderExecutor.execute(PACKAGE_NAME, EntityMatchers.getMatchers());
     }
 }

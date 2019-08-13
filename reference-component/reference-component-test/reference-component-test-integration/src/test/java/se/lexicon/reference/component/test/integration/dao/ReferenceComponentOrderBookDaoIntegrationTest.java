35df3dc79db28caebb7503eb73bcc293ceb1aa09
shazahlaleh <shazahilalh@gmail.com>
2019-08-13 16:05:43 +0200

    fixed clearing gigaspace for new entities in OrderBookDaoIntegration.

--- reference-component/reference-component-test/reference-component-test-integration/src/test/java/se/lexicon/reference/component/test/integration/dao/ReferenceComponentOrderBookDaoIntegrationTest.java
+++ reference-component/reference-component-test/reference-component-test-integration/src/test/java/se/lexicon/reference/component/test/integration/dao/ReferenceComponentOrderBookDaoIntegrationTest.java
@@ -1,14 +1,18 @@
 package se.lexicon.reference.component.test.integration.dao;
 
 import com.so4it.test.category.IntegrationTest;
+import com.so4it.test.gs.rule.ClearGigaSpaceTestRule;
 import org.junit.Assert;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.rules.RuleChain;
+import org.openspaces.core.GigaSpace;
 import se.lexicon.reference.component.dao.OrderBookDao;
 import se.lexicon.reference.component.entity.OrderBookEntity;
 import se.lexicon.reference.component.test.common.entity.OrderBookEntityTestBuilder;
+import se.lexicon.reference.component.test.integration.service.ReferenceComponentServiceIntegrationTestSuite;
 
 
 @Category(IntegrationTest.class)
@@ -17,8 +21,8 @@ public class ReferenceComponentOrderBookDaoIntegrationTest {
     @ClassRule
     public static final RuleChain SUITE_RULE_CHAIN = ReferenceComponentDaoIntegrationTestSuite.SUITE_RULE_CHAIN;
 
-    //@Rule
-    //public ClearGigaSpaceTestRule clearGigaSpaceTestRule = new ClearGigaSpaceTestRule(ReferenceComponentServiceIntegrationTestSuite.getExportContext().getBean(GigaSpace.class));
+    @Rule
+    public ClearGigaSpaceTestRule clearGigaSpaceTestRule = new ClearGigaSpaceTestRule(ReferenceComponentServiceIntegrationTestSuite.getExportContext().getBean(GigaSpace.class));
 
 
 
